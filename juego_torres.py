ea11ff421eb9c0344b581cedf1da9568e9f643da
albabernal03 <ALBABERO@MYUAX.COM>
2021-12-14 12:05:07 +0100

    corrigo los errores de las parte de abajo, además elimino y añado

--- juego_torres.py
+++ juego_torres.py
@@ -37,8 +37,8 @@ def verticalRooks(r1, r2):
         return ('ha ganado el jugador 2!')
 
 
- if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+if __name__ == '__main__':
+    
     t = int(input().strip())
     for t_itr in range(t):
         n = int(input().strip())
@@ -51,6 +51,4 @@ def verticalRooks(r1, r2):
             posicion_torre_2 = int(input().strip())
             r2.append(posicion_torre_2)
         result = verticalRooks(r1, r2)
-        fptr.write(result + '\n')
-
- fptr.close()
+        print.write(result + '\n')
